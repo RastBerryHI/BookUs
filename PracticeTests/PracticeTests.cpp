8641c605b1b10cc7da1e4a9cdc61a73e2037e9ff
RastBerryHI <musaev.pasha@gmail.com>
2021-06-20 12:35:42 +0300

    All modules

---
+++ PracticeTests/PracticeTests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PracticeProj/MyForm.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace PracticeProj;
+
+namespace PracticeTests
+{
+	TEST_CLASS(PracticeTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			PracticeProj::MyFrom fm;
+		}
+	};
+}
